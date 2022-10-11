fde710a287a943efabf4d0cd75615a3cf7fe8fc2
OlhaAleks <106926651+OlhaAleks@users.noreply.github.com>
2022-10-11 14:54:29 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>
+     Weather App 
+     </h1>
+     <footer>
+     This project was coded by <a href="https://ubiquitous-swan-8b46e3.netlify.app/about.html">Olha Aleksandrova</a>  and is 
+     <a href="https://github.com/OlhaAleks/react-project-weather"> open-sourced on Git-Hub</a>
+     </footer>
+    </div>
+  );
+}
+
