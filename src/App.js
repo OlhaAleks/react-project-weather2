f60ca4594c93c0a3403bc5c99059ff195b0efa9d
OlhaAleks <106926651+OlhaAleks@users.noreply.github.com>
2022-10-11 17:23:48 +0300

    Added new component Weather

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import React from 'react';
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
       <h1>
      Weather App 
      </h1>
+        <Weather />
      <footer>
      This project was coded by <a href="https://ubiquitous-swan-8b46e3.netlify.app/about.html">Olha Aleksandrova</a>  and is 
      <a href="https://github.com/OlhaAleks/react-project-weather2"> open-sourced on Git-Hub</a>
      </footer>
+     </div>
     </div>
   );
 }
